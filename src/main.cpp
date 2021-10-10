8c02fcd1c5b3b06e93cf5a4bda70e3c295ce00ee
diaxz <dias.arifin@gmail.com>
2021-10-10 10:39:04 +0700

    blink build in led

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,24 @@
 
 BlynkTimer timer;
 
+BLYNK_WRITE(V0)
+{
+    int value = param.asInt();
+    Blynk.virtualWrite(V1, value);
+    if (value == 0) {
+        digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+}
+
+BLYNK_CONNECTED()
+{
+    Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+    Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+    Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
 void timerEvent()
 {
     Blynk.virtualWrite(V2, millis() / 1000);
@@ -12,6 +30,8 @@ void setup()
     Serial.begin(115200);
     Blynk.begin(auth, ssid, pass);
     timer.setInterval(1000L, timerEvent);
+
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
